a0d37ba90be7da536641fd73feb37d57837b1b98
Abhishek Bhardwaj <80835016+Abhishek-bhardwajj@users.noreply.github.com>
2023-01-30 10:18:41 +0530

    Time: 0 ms (100.00%), Space: 65.3 MB (95.66%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
@@ -1,19 +1,44 @@
-class Solution {
-    public int findCenter(int[][] edges) {
-        int n = edges.length + 1;
-        int[] edgeCount = new int[n+1];
+//Bruteforce approach:
+
+// class Solution {
+//     public int findCenter(int[][] edges) {
+//         int n = edges.length + 1;
+//         int[] edgeCount = new int[n+1];
+//         for(int[] edge : edges){
+//             int x = edge[0];
+//             int y = edge[1];
+//             edgeCount[x]++;
+//             edgeCount[y]++;
+//             if(edgeCount[x]>1){
+//                 return x;
+//             }
+//             if(edgeCount[y]>1){
+//                 return y;
+//             }
+//         }
+//         return 0;
+//     }
+// }
+
+
+
+//Optimised:
+
+import java.util.HashSet;
+import java.util.Set;
+class Solution{
+    public int findCenter(int[][] edges){
+        Set<Integer> outEdgesFound = new HashSet<>();
         for(int[] edge : edges){
-            int x = edge[0];
-            int y = edge[1];
-            edgeCount[x]++;
-            edgeCount[y]++;
-            if(edgeCount[x]>1){
-                return x;
+            if(outEdgesFound.contains(edge[0])){
+                return edge[0];
             }
-            if(edgeCount[y]>1){
-                return y;
+            outEdgesFound.add(edge[0]);
+            if(outEdgesFound.contains(edge[1])){
+                return edge[1];
             }
+            outEdgesFound.add(edge[1]);
         }
-        return 0;
+        return -1;
     }
 }
