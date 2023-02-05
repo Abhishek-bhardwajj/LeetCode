3432f1584670ef20e601b8fba974b70ea964e929
Abhishek Bhardwaj <80835016+Abhishek-bhardwajj@users.noreply.github.com>
2023-02-06 01:31:48 +0530

    Time: 6 ms (71.45%), Space: 50.3 MB (78.09%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,23 +1,42 @@
+// public class Solution {
+//     public List<Integer> findDisappearedNumbers(int[] nums) {
+//         int i = 0;
+//         while(i < nums.length) {
+//             if (nums[i] == i + 1) {
+//                 i ++;
+//                 continue;
+//             }
+//             int index = nums[i] - 1;
+//             if (nums[index] == index + 1) {
+//                 i ++;
+//                 continue;
+//             }
+//             nums[i] = nums[index];
+//             nums[index] = index + 1;
+//         }
+//         List<Integer> result = new LinkedList<Integer>();
+//         for (int j = 0; j < nums.length; j ++) {
+//             if (nums[j] != j + 1) {
+//                 result.add(j + 1);
+//             }
+//         }
+//         return result;
+//     }
+// }
+
+
 public class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        int i = 0;
-        while(i < nums.length) {
-            if (nums[i] == i + 1) {
-                i ++;
-                continue;
-            }
-            int index = nums[i] - 1;
-            if (nums[index] == index + 1) {
-                i ++;
-                continue;
-            }
-            nums[i] = nums[index];
-            nums[index] = index + 1;
+        for (int i = 0; i < nums.length; i ++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] > 0) {
+                nums[index] = - nums[index];
+            }        
         }
         List<Integer> result = new LinkedList<Integer>();
-        for (int j = 0; j < nums.length; j ++) {
-            if (nums[j] != j + 1) {
-                result.add(j + 1);
+        for (int i = 0; i < nums.length; i ++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
             }
         }
         return result;
