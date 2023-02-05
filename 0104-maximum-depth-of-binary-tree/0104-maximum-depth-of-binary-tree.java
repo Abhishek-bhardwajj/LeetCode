e8387e52d2672a68bdf377d5e69cfdd11ed46a2e
Abhishek Bhardwaj <80835016+Abhishek-bhardwajj@users.noreply.github.com>
2023-02-05 22:26:59 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (94.99%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -16,27 +16,39 @@
 
 //Iterative Appraoch
 
+// public class Solution {
+//     public int maxDepth(TreeNode root) {
+//         if (root == null) {
+//             return 0;
+//         }
+//         Queue<TreeNode> q = new LinkedList<TreeNode>();
+//         q.add(root);
+//         int count = 0;
+//         while (q.size() > 0) {
+//             int n = q.size();
+//             count++;
+//             for (int i = 0; i < n; i ++) {
+//                 TreeNode node = q.remove();
+//                 if (node.left != null) {
+//                     q.add(node.left);
+//                 }
+//                 if (node.right !=null) {
+//                     q.add(node.right);
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }
+
+
+//Recurasive Approach
+
 public class Solution {
     public int maxDepth(TreeNode root) {
         if (root == null) {
             return 0;
         }
-        Queue<TreeNode> q = new LinkedList<TreeNode>();
-        q.add(root);
-        int count = 0;
-        while (q.size() > 0) {
-            int n = q.size();
-            count++;
-            for (int i = 0; i < n; i ++) {
-                TreeNode node = q.remove();
-                if (node.left != null) {
-                    q.add(node.left);
-                }
-                if (node.right !=null) {
-                    q.add(node.right);
-                }
-            }
-        }
-        return count;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 }
