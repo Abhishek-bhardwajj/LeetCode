7a13b163c6ec80dc9f34b23c5790b1b550a96875
Abhishek Bhardwaj <80835016+Abhishek-bhardwajj@users.noreply.github.com>
2023-09-30 21:37:05 +0530

    Time: 0 ms (100.00%), Space: 41 MB (14.19%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -10,27 +10,44 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        if(head == null){
-            return null;
-        }
-        int size = 0;
-        ListNode curr = head;
-        while(curr != null){
-            size++;
-            curr = curr.next;
-        }
+//         if(head == null){
+//             return null;
+//         }
+//         int size = 0;
+//         ListNode curr = head;
+//         while(curr != null){
+//             size++;
+//             curr = curr.next;
+//         }
         
-        if( n == size){
-            return head.next;
+//         if( n == size){
+//             return head.next;
+//         }
+//         int indexToSearch = size - n;
+//         ListNode prev = head;
+//         int i=1;
+//         while(i < indexToSearch){
+//             prev = prev.next;
+//             i++;
+//         }
+//         prev.next = prev.next.next;
+//         return head;
+        
+        
+        
+        //Two pointer approach(Optimal Approach)
+        
+        ListNode fast = head, slow = head;
+        for(int i=0;i<n;i++){
+            fast = fast.next;
         }
-        int indexToSearch = size - n;
-        ListNode prev = head;
-        int i=1;
-        while(i < indexToSearch){
-            prev = prev.next;
-            i++;
+        if(fast == null) return head.next;
+        while(fast.next != null){
+            fast = fast.next;
+            slow = slow.next;
         }
-        prev.next = prev.next.next;
+        slow.next = slow.next.next;
         return head;
+        
     }
 }
