93827b05ae4dddc218dc76e90033e57863a22108
Abhishek Bhardwaj <80835016+Abhishek-bhardwajj@users.noreply.github.com>
2023-12-20 16:20:47 +0530

    Time: 0 ms (100.00%), Space: 45.6 MB (6.74%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -42,51 +42,75 @@
 //     }
 // }
 
+
+
+//This solution is even correct for the negative values also.
+// class Solution {
+//     public void setZeroes(int[][] matrix) {
+//         int n = matrix.length;
+//         int m = matrix[0].length;
+
+//         // Create a boolean matrix to mark cells that need to be set to zero
+//         boolean[][] zeroMarker = new boolean[n][m];
+
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < m; j++) {
+//                 if (matrix[i][j] == 0) {
+//                     markRowAndCol(i, j, matrix, zeroMarker);
+//                 }
+//             }
+//         }
+
+//         // Set the marked cells to zero
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < m; j++) {
+//                 if (zeroMarker[i][j]) {
+//                     matrix[i][j] = 0;
+//                 }
+//             }
+//         }
+//     }
+
+//     public static void markRowAndCol(int row, int col, int[][] matrix, boolean[][] zeroMarker) {
+//         int n = matrix.length;
+//         int m = matrix[0].length;
+
+//         // Mark the entire row
+//         for (int i = 0; i < n; i++) {
+//             zeroMarker[i][col] = true;
+//         }
+
+//         // Mark the entire column
+//         for (int j = 0; j < m; j++) {
+//             zeroMarker[row][j] = true;
+//         }
+//     }
+// }
+
+// 2. Better approach (tc - O(2*n*m))
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-
-        // Create a boolean matrix to mark cells that need to be set to zero
-        boolean[][] zeroMarker = new boolean[n][m];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == 0) {
-                    markRowAndCol(i, j, matrix, zeroMarker);
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        int row[] = new int[m];
+        int col[] = new int[n];
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j] == 0){
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
-
-        // Set the marked cells to zero
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (zeroMarker[i][j]) {
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(row[i] == 1 || col[j] == 1){
                     matrix[i][j] = 0;
                 }
             }
         }
     }
-
-    public static void markRowAndCol(int row, int col, int[][] matrix, boolean[][] zeroMarker) {
-        int n = matrix.length;
-        int m = matrix[0].length;
-
-        // Mark the entire row
-        for (int i = 0; i < n; i++) {
-            zeroMarker[i][col] = true;
-        }
-
-        // Mark the entire column
-        for (int j = 0; j < m; j++) {
-            zeroMarker[row][j] = true;
-        }
-    }
 }
-
-
-// class Solution {
-//     public void setZeroes(int[][] matrix) {
-        
-//     }
-// }
