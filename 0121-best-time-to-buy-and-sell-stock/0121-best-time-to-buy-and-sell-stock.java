9faec863329cd9406e4c2e71213f02b41bb03c8e
Abhishek Bhardwaj <80835016+Abhishek-bhardwajj@users.noreply.github.com>
2023-12-21 16:07:18 +0530

    Time: 3 ms (10.05%), Space: 61.9 MB (5.36%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,37 @@
+// 1. Brute force approach O(n^2)   Applicable only for the smaller/ limited size array values.
+
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int max = 0;
+//         int N = prices.length;
+
+//         for (int i = 0; i < N - 1; i++) {
+//             int buyPrice = prices[i];
+
+//             for (int j = i + 1; j < N; j++) {
+//                 int sellPrice = prices[j];
+//                 int profit = sellPrice - buyPrice;
+
+//                 if (profit > 0) {
+//                     max = Math.max(max, profit);
+//                 }
+//             }
+//         }
+
+//         return max;
+//     }
+// }
+
+
+// 2. Optimal solution O(n)
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxPro = 0;
+        int minPrice = prices[0];
+        for(int i=0;i<prices.length;i++){
+            minPrice = Math.min(minPrice, prices[i]);
+            maxPro  = Math.max(maxPro, prices[i]-minPrice);
+        }
+        return maxPro;
+    }
+}
